b58882cfb4c8a5618a50964a41655b9d3396779c
Tape <160833081+YuChiLiang@users.noreply.github.com>
2024-02-23 17:16:57 +0800

    Add files via upload

---
+++ Week01 HW/name.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>myWeb</title>
+    <style>
+        h1{
+            margin: 0;
+        }
+    </style>
+</head>
+<body>
+    <h1>Not Found</h1>
+    <p>The requested URL was not found on this server.</p>
+    <hr>
+    <address>Apache/8.7.87 (Win64) OpenSSL/6.6.6 PHP/8.7.87 Server at localhost Port 87</address>
+</body>
+</html>
